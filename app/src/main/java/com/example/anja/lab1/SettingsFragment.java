71314652f51c34840d4df88eacf1c5f1b5dd6ca0
Anja Subasic <anja.subasic@gmail.com>
2017-09-24 17:53:18 -0400

    Added navigation tabs

---
+++ app/src/main/java/com/example/anja/lab1/SettingsFragment.java
@@ -0,0 +1,25 @@
+package com.example.anja.lab1;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Anja on 9/24/2017.
+ */
+
+public class SettingsFragment extends Fragment {
+    private static final String TAG = "Settings";
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.settings_fragment, container,false);
+
+        return view;
+    }
+
+}
