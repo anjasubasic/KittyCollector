d85eca69578729aee364c3bdacf03ff95bc2a63c
Anja Subasic <anja.subasic@gmail.com>
2017-09-26 14:27:38 -0400

    Added clear button and required functionality

--- app/src/main/java/com/example/anja/lab1/SettingsFragment.java
+++ app/src/main/java/com/example/anja/lab1/SettingsFragment.java
@@ -3,10 +3,13 @@ package com.example.anja.lab1;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentManager;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 
 /**
@@ -22,6 +25,25 @@ public class SettingsFragment extends android.support.v4.app.DialogFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.settings_fragment, container,false);
+        final Button clearButton = (Button) view.findViewById(R.id.clear_button);
+        final Button saveButton = (Button) view.findViewById(R.id.save_button);
+
+        clearButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        final EditText charTxtEdit = (EditText) getActivity().findViewById(R.id.character_edit_text);
+                        final EditText nameTxtEdit = (EditText) getActivity().findViewById(R.id.name_edit_text);
+                        final EditText passwordTxtEdit = (EditText) getActivity().findViewById(R.id.password_edit_text);
+
+                        charTxtEdit.getText().clear();
+                        nameTxtEdit.getText().clear();
+                        passwordTxtEdit.getText().clear();
+
+                        // disable save button if all input was clear. Also need to set the passwordsMatch boolean back to false
+                        saveButton.setEnabled(false);
+                    }
+                });
 
         final EditText passwordTxtEdit = (EditText) view.findViewById(R.id.password_edit_text);
 
@@ -40,6 +62,74 @@ public class SettingsFragment extends android.support.v4.app.DialogFragment {
             }
         });
 
+        setClearButtonVisibility(view);
+
         return view;
+    }
+
+    private void setClearButtonVisibility(View view) {
+
+        //TODO: We should clean this up if we can. I didn't know how to create a TextWatcher for multiple EditTexts so I just left it like this for now.
+
+        final EditText charTxtEdit = (EditText) view.findViewById(R.id.character_edit_text);
+        final EditText nameTxtEdit = (EditText) view.findViewById(R.id.name_edit_text);
+        final EditText passwordTxtEdit = (EditText) view.findViewById(R.id.password_edit_text);
+        final Button clearButton = view.findViewById(R.id.clear_button);
+
+        charTxtEdit.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) { }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if (charTxtEdit.getText().length() == 0 && nameTxtEdit.getText().length() == 0 && passwordTxtEdit.getText().length() == 0)
+                    clearButton.setVisibility(View.INVISIBLE);
+                else
+                    clearButton.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) { }
+        });
+
+        nameTxtEdit.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if (charTxtEdit.getText().length() == 0 && nameTxtEdit.getText().length() == 0 && passwordTxtEdit.getText().length() == 0)
+                    clearButton.setVisibility(View.INVISIBLE);
+                else
+                    clearButton.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        passwordTxtEdit.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if (charTxtEdit.getText().length() == 0 && nameTxtEdit.getText().length() == 0 && passwordTxtEdit.getText().length() == 0)
+                    clearButton.setVisibility(View.INVISIBLE);
+                else
+                    clearButton.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
     }
 }
