f51789aa0349db57918298f9c4a3a5fafd34b86c
Jenny (Hyun Ji) Seong <jennyseong77@gmail.com>
2017-10-12 01:02:44 -0400

    🐛 fixed typo

--- Lab 2/app/src/main/java/com/example/anja/lab1/RegisterAccountFragment.java
+++ Lab 2/app/src/main/java/com/example/anja/lab1/RegisterAccountFragment.java
@@ -293,6 +293,7 @@ public class RegisterAccountFragment extends Fragment {
 
                 // Delete temporary image taken by camera after crop.
                 if (isTakenFromCamera) {
+
                     File f = new File(imageUri.getPath());
                     if (f.exists())
                         f.delete();
