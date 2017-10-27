ef7d26f285a55671b3f9a63fe5e4d90c1f35a1db
Jenny (Hyun Ji) Seong <jennyseong77@gmail.com>
2017-10-27 01:12:58 -0400

    😺 Update number of cats in list on plat frament

--- Lab 3/app/src/main/java/com/example/anja/lab1/PlayFragment.java
+++ Lab 3/app/src/main/java/com/example/anja/lab1/PlayFragment.java
@@ -1,18 +1,26 @@
 package com.example.anja.lab1;
 
-import android.app.PendingIntent;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.TaskStackBuilder;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
 
 /**
  * Created by Anja on 9/24/2017.
@@ -22,6 +30,7 @@ public class PlayFragment extends Fragment {
     private static final String TAG = "Play";
     private TextView helloTxt, scoreTxt;
     private Button playButton;
+    private int numCats;
 
     @Nullable
     @Override
@@ -31,6 +40,7 @@ public class PlayFragment extends Fragment {
         helloTxt = view.findViewById(R.id.helloText);
         scoreTxt = view.findViewById(R.id.scoreText);
         playButton = view.findViewById(R.id.playButton);
+        numCats = 0;
 
         loadUserInfo();
 
@@ -48,14 +58,46 @@ public class PlayFragment extends Fragment {
     private void loadUserInfo() {
         SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(getActivity());
         String helloMessage = "Hi, @" + sp.getString("username", "") + "!";
-        String scoreTrack = "You have befriended " + sp.getString("score", "0") + " cats.";
         helloTxt.setText(helloMessage);
-        scoreTxt.setText(scoreTrack);
+
+        getCatNum();
 
         if (sp == null) {
             helloTxt.setText(R.string.helloMessage);
             scoreTxt.setText(R.string.scoreTrack);
         }
+    }
+
+    private void getCatNum() {
+        RequestQueue queue = Volley.newRequestQueue(getActivity());
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        String username = sp.getString("username", "");
+        String password = sp.getString("password", "");
+        Boolean hardMode = sp.getBoolean("hard", false);
+
+        // set cat list request URL according to game mode
+        String url ="http://cs65.cs.dartmouth.edu/catlist.pl?name=";
+        String requestUrl;
+        if (hardMode) requestUrl = url + username + "&password=" + password + "&mode=hard";
+        else requestUrl = url + username + "&password=" + password + "&mode=easy";
+
+        final JsonArrayRequest jsObjRequest = new JsonArrayRequest (Request.Method.GET,
+                requestUrl, null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        numCats = response.length();
+                        String scoreTrack = "You have " + numCats + " cats waiting!";
+                        scoreTxt.setText(scoreTrack);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getActivity(), "Error: " + error.toString(),
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+        queue.add(jsObjRequest);
     }
 
 }
